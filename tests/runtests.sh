173168ee6eab59b941eda146c448b45c6c5c6864
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-12-14 21:24:25 +0100

    better runtests.log-filename and prevent default args (and sound)

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -5,6 +5,12 @@ then
  PD=pd
 fi
 
+if [ "x${PDARGS}" = "x" ]
+then
+ PDARGS="-noprefs -nosound -nrt"
+fi
+
+
 RUNTESTS_TXT=runtests.txt
 RUNTESTS_LOG=runtests.log
 
@@ -13,12 +19,12 @@ ls -1 */*.pd | sed 's/\.pd/;/' > $RUNTESTS_TXT
 IEMMATRIX="-lib ../iemmatrix -path ../abs/"
 
 run_nogui() {
- pd $IEMMATRIX -nogui runtests_nogui.pd > ${RUNTESTS_LOG}.$$ 2>&1 
+ ${PD} ${PDARGS} $IEMMATRIX -nogui runtests_nogui.pd > ${RUNTESTS_LOG} 2>&1
  NUMTESTS=`grep -c . $RUNTESTS_TXT`
- echo "regression-test: ${NUMTESTS} tests total" >>  ${RUNTESTS_LOG}.$$
- 
- cat ${RUNTESTS_LOG}.$$ | egrep "^regression-test: " | sed -e 's/^regression-test: //'
- FAILEDTESTS=$(cat ${RUNTESTS_LOG}.$$ | egrep "^regression-test: .*: failed$" | sed -e 's|^regression-test: ||' -e 's|: failed$||')
+ echo "regression-test: ${NUMTESTS} tests total" >>  ${RUNTESTS_LOG}
+
+ cat ${RUNTESTS_LOG} | egrep "^regression-test: " | sed -e 's/^regression-test: //'
+ FAILEDTESTS=$(cat ${RUNTESTS_LOG} | egrep "^regression-test: .*: failed$" | sed -e 's|^regression-test: ||' -e 's|: failed$||')
  echo -n "failed tests: "
  for ft in ${FAILEDTESTS}; do
    echo -n "${ft} "
@@ -27,12 +33,13 @@ run_nogui() {
 }
 
 run_withgui() {
- pd $IEMMATRIX -stderr runtests.pd 2>&1 | tee ${RUNTESTS_LOG}
+ ${PD} ${PDARGS} $IEMMATRIX -stderr runtests.pd 2>&1 | tee ${RUNTESTS_LOG}
 }
 
 if test "x$1" = "x-gui"; then
  run_withgui
 else
+ RUNTESTS_LOG=${RUNTESTS_LOG}.$(date +%Y%m%d-%H%M).$$
  run_nogui
 fi
 

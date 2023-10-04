6860b0c367a60e80b1688adfcf9ef97af20ced57
IOhannes m zmölnig <zmoelnig@umlautT.umlaeute.mur.at>
2023-10-04 17:58:24 +0200

    First working version

--- src/mtx_mul~.c
+++ src/mtx_mul~.c
@@ -63,24 +63,30 @@ typedef struct matrix_multilde {
   int           x_compat; /* 0=mtx_*~; 1=matrix_mul_line~; 2=matrix~ */
   setmultiout_f x_setmultiout; /* when doing multichannel, this is Pd>=0.54's signal_setmultiout(); otherwise NULL */
 
+  /* DSP meta information */
   t_sample	**x_io; /* input/output signals (for easier passing to perform()) */
   t_float	x_msi; /* CLASS_MAINSIGNALIN() */
-
   int           x_dsp; /* is the DSP running? */
+
+   /* number of non-multichannels iolets.
+    * a value of '0' indicates a single multichannel iolets
+    */
+  size_t        x_inports, x_outports;
+
+  /* the matrix */
+  /* while DSP is running, rows=outs, cols=ins;
+   * if XXports>0, then the rows/cols must match when setting a matrix
+   */
+  size_t        x_rows, x_cols;
+  t_float       *x_matcur; /* current matrix (being interpolated) */
+  t_float	*x_matend; /* interpolation target */
 
   /* interpolation data */
   t_float	x_time_ms; /* interpolation time (when new matrix comes through) */
   int		x_remaining_ticks; /* how long do we still need to interpolate */
-  t_float	*x_inc, *x_biginc; /* incrementation values */
+  t_float	*x_inc, *x_biginc; /* matrices incrementation values */
   int		x_retarget; /* bool: whether we need to start a new interpolation */
   t_float	x_ms2tick; /* helper to translate time to ticks */
-
-
-  /* the matrix */
-  t_float       *x_matcur; /* current matrix (being interpolated) */
-  t_float	*x_matend; /* interpolation target */
-  int		x_n_in;	/* columns */
-  int		x_n_out; /* rows */
 
   /* buffer for matrix multiplication */
   t_sample	*x_outsumbuf; /* N samples for summing up */
@@ -91,7 +97,46 @@ static void proxy_dspstopped(t_proxy*p) {
   p->p_owner->x_dsp = 0;
 }
 
+static t_float*get_resized_matrix(const t_float*src,
+				  const unsigned int srcR, const unsigned int srcC,
+				  const unsigned int dstR, const unsigned int dstC) {
+  t_float*dst = (t_float*)getbytes(dstR*dstC*sizeof(*dst));
+  unsigned int c, r, cols, rows;
+  if(!dst)
+    return dst;
+  cols = (srcC<dstC)?srcC:dstC;
+  rows = (srcR<dstR)?srcR:dstR;
+  for(r=0; r<rows; r++) {
+    for(c=0; c<cols; c++) {
+      dst[dstC*r + c] = src[srcC*r + c];
+    }
+  }
+  return dst;
+}
+static t_float*resize_and_free(t_float*src,
+			       const unsigned int srcR, const unsigned int srcC,
+			       const unsigned int dstR, const unsigned int dstC) {
+  t_float*dummy = get_resized_matrix(src, srcR, srcC, dstR, dstC);
+  freebytes(src, srcR*srcC*sizeof(*src));
+  return dummy;
+}
 
+static int matrix_multilde_resize(t_matrix_multilde *x, unsigned int rows, unsigned int cols) {
+  if((rows == x->x_rows) && (cols == x->x_cols))
+    return 1;
+  x->x_matend = resize_and_free(x->x_matend, x->x_rows, x->x_cols, rows, cols);
+  x->x_matcur = resize_and_free(x->x_matcur, x->x_rows, x->x_cols, rows, cols);
+  x->x_inc    = resize_and_free(x->x_inc   , x->x_rows, x->x_cols, rows, cols);
+  x->x_biginc = resize_and_free(x->x_biginc, x->x_rows, x->x_cols, rows, cols);
+  if(!x->x_matend || !x->x_matcur || !x->x_inc || !x->x_biginc) {
+    pd_error(x, "[mtx_*~] failed to resize matrices to [%dx%d]", rows, cols);
+    x->x_rows = x->x_cols = 0;
+    return 0;
+  }
+  x->x_rows = rows;
+  x->x_cols = cols;
+  return 1;
+}
 
 
 static t_class *matrix_multilde_mclass;
@@ -109,12 +154,11 @@ static void matrix_multilde_time(t_matrix_multilde *x, t_floatarg time_ms)
 static void matrix_multilde_matrix_set(t_matrix_multilde *x, int argc,
                                        t_atom *argv, int transpose)
 {
-  int col, row, i, length;
-  t_float *matcur = x->x_matcur;
-  t_float *matend = x->x_matend;
+  int col, row, length;
+  t_float *matcur, *matend;
 
   if(argc<2) {
-    pd_error(x, "[mtx_*~]: bad matrix: <int:out_rows> <int:in_cols> !");
+    pd_error(x, "[mtx_*~]: bad matrix: <int:out_rows> <int:ix_cols> !");
     return;
   }
   if(iemmatrix_check(x, argc, argv, 0))return;
@@ -131,29 +175,50 @@ static void matrix_multilde_matrix_set(t_matrix_multilde *x, int argc,
     col=dummy;
   }
 
-  if((col!=x->x_n_in)||(row!=x->x_n_out)) {
-    pd_error(x,"[mtx_*~]: matrix dimensions do not match (%dx%d != %dx%d)!!",
-             col, row, x->x_n_in, x->x_n_out);
-    return;
+  if (x->x_dsp) {
+    /* matrix dimensions must match while DSP is running */
+    if ((col != x->x_cols) || (row != x->x_rows)) {
+      pd_error(x,"[mtx_*~]: matrix dimensions must not change (%dx%d != %dx%d) while DSP is running!!",
+	       col, row, x->x_cols, x->x_rows);
+      return;
+    }
+  } else {
+    /* DSP is not running, check if we have a fixed number of iolets */
+    if(x->x_inports && x->x_inports != col) {
+      pd_error(x, "[mtx_*~]: cannot change fixed number of input channels (%d) to %d", x->x_inports, col);
+      return;
+    }
+    if(x->x_outports && x->x_outports != row) {
+      pd_error(x, "[mtx_*~]: cannot change fixed number of output channels (%d) to %d", x->x_outports, row);
+      return;
+    }
+
+    if(!matrix_multilde_resize(x, row, col))
+      return;
   }
 
+  matcur = x->x_matcur;
+  matend = x->x_matend;
   length = col * row;
 
   if(transpose) {
     /* we need to transpose the matrix */
+    int i;
     for(i=0; i<row; i++) {
-      int j=0;
+      int j;
       for(j=0; j<col; j++) {
         *matend++=atom_getfloat(argv+i+j*row);
       }
     }
   } else {
+    int i;
     for(i=0; i<length; i++) {
       *matend++=atom_getfloat(argv++);
     }
   }
 
   if(x->x_time_ms <= 0.0f) {
+    int i;
     matend = x->x_matend;
     for(i=0; i<length; i++) {
       *matcur++=*matend++;
@@ -177,14 +242,13 @@ static void matrix_multilde_matrixT(t_matrix_multilde *x, t_symbol *s,
 static void matrix_multilde_element(t_matrix_multilde *x, t_symbol *s,
                                     int argc, t_atom *argv)
 {
-  int col, row, n_in_cols=x->x_n_in;
+  int col, row, n_ix_cols=x->x_cols;
   t_float element;
   t_float *matcur = x->x_matcur;
   t_float *matend = x->x_matend;
 
   if(argc != 3) {
-    pd_error(x,
-             "mtx_*~ : bad element: 3 floats: <int> out_row <int> in_col <float> element !");
+    pd_error(x, "[mtx_*~]: bad arguments, expected <int:row> <int:column> <float:value>!");
     return;
   }
 
@@ -192,17 +256,17 @@ static void matrix_multilde_element(t_matrix_multilde *x, t_symbol *s,
   col = atom_getint(argv+1) - 1;
   element = atom_getfloat(argv+2);
 
-  if((row >= x->x_n_out) || (row < 0)) {
-    pd_error(x,"mtx_*~ : row dimensions do not match !!");
+  if((row >= x->x_rows) || (row < 0)) {
+    pd_error(x, "[mtx_*~]: out of bound row!!");
     return;
   }
-  if((col >= n_in_cols) || (col < 0)) {
-    pd_error(x,"mtx_*~ : col dimensions do not match !!");
+  if((col >= n_ix_cols) || (col < 0)) {
+    pd_error(x, "[mtx_*~]: out of bound column!!");
     return;
   }
 
-  matend += row * n_in_cols + col;
-  matcur += row * n_in_cols + col;
+  matend += row * n_ix_cols + col;
+  matcur += row * n_ix_cols + col;
 
   if(x->x_time_ms <= 0.0f) {
     *matend = *matcur = element;
@@ -221,7 +285,7 @@ static void matrix_multilde_row(t_matrix_multilde *x, t_symbol *s,
   t_float *matend = x->x_matend;
 
   if(argc<1) {
-    pd_error(x,"mtx_*~ : bad row: <int> in_row !");
+    pd_error(x,"[mtx_*~]: bad row!");
     return;
   }
 
@@ -229,13 +293,13 @@ static void matrix_multilde_row(t_matrix_multilde *x, t_symbol *s,
   argv++;
   argc--;
 
-  if((nth_row >= x->x_n_out) || (nth_row < 0)) {
-    pd_error(x,"mtx_*~ : row dimensions do not match !!");
+  if((nth_row >= x->x_rows) || (nth_row < 0)) {
+    pd_error(x, "[mtx_*~]: out of bound row!!");
     return;
   }
-  col = x->x_n_in;
+  col = x->x_cols;
   if(argc < col) {
-    pd_error(x,"mtx_*~ : col dimensions do not match !!");
+    pd_error(x,"[mtx_*~]: col dimensions do not match !!");
     return;
   }
 
@@ -264,7 +328,7 @@ static void matrix_multilde_col(t_matrix_multilde *x, t_symbol *s,
   t_float *matend = x->x_matend;
 
   if(argc<1) {
-    pd_error(x,"mtx_*~ : bad col: <int> in_cols !");
+    pd_error(x,"[mtx_*~]: bad column!");
     return;
   }
 
@@ -272,14 +336,14 @@ static void matrix_multilde_col(t_matrix_multilde *x, t_symbol *s,
   argv++;
   argc--;
 
-  col = x->x_n_in;
+  col = x->x_cols;
   if((nth_col < 0)||(nth_col >= col)) {
-    pd_error(x,"mtx_*~ : col dimensions do not match !!");
+    pd_error(x, "[mtx_*~]: out of bound column!!");
     return;
   }
-  row = x->x_n_out;
+  row = x->x_rows;
   if(argc < row) {
-    pd_error(x,"mtx_*~ : row dimensions do not match !!");
+    pd_error(x,"[mtx_*~]: row dimensions do not match !!");
     return;
   }
 
@@ -306,7 +370,7 @@ static void matrix_multilde_col(t_matrix_multilde *x, t_symbol *s,
 
 static void matrix_multilde_stop(t_matrix_multilde *x)
 {
-  int i = x->x_n_out*x->x_n_in;
+  int i = x->x_rows*x->x_cols;
   t_float *matend=x->x_matend;
   t_float *matcur=x->x_matcur;
 
@@ -326,8 +390,8 @@ static t_int *matrix_multilde_perf8(t_int *w)
   t_sample *outsum, *houtsum;
   t_float *matcur, *matend;
   t_float *inc1 ,*biginc, inc;
-  int n_in = x->x_n_in;   /* columns */
-  int n_out = x->x_n_out; /* rows	*/
+  int n_in = x->x_cols;
+  int n_out = x->x_rows;
   t_float *in, *out, mul, bigmul;
   int r, c, i;
 
@@ -587,8 +651,8 @@ static t_int *matrix_multilde_perform(t_int *w)
   t_sample *outsum, *houtsum;
   t_float *matcur, *matend;
   t_float *inc1 ,*biginc, inc;
-  int n_in = x->x_n_in;   /* columns */
-  int n_out = x->x_n_out; /* rows	*/
+  int n_in = x->x_cols;
+  int n_out = x->x_rows;
   t_float *in, *out, mul, bigmul;
   int r, c, i;
 
@@ -747,15 +811,56 @@ static t_int *matrix_multilde_perform(t_int *w)
 }
 static void matrix_multilde_dsp(t_matrix_multilde *x, t_signal **sp)
 {
-  int i, n=sp[0]->s_n * x->x_n_out;
+  const int length = sp[0]->s_n;
+  int i, n=length * x->x_rows;
   /* [mtx_*~] ignores the signal on the very 1st inlet */
   int compat_offset=(x->x_compat)?0:1;
-  int ichannels = x->x_n_in;
-  int ochannels = x->x_n_out;
+  int ichannels = x->x_inports?x->x_inports:0;
+  int ochannels = x->x_outports?x->x_outports:x->x_rows;
+  t_sample**io;
 
   /* DSP is running */
   x->x_dsp = 1;
 
+  if(x->x_setmultiout) {
+#if CLASS_MULTICHANNEL
+    /* multichannel mode */
+    if(!ichannels) {
+      /* multichannel input */
+      ichannels = sp[compat_offset]->s_nchans;
+    }
+
+    if(x->x_outports) {
+      /* create singlechannel outputs */
+      for(i=0; i<ochannels; i++) {
+	x->x_setmultiout(&sp[compat_offset + ichannels + i], 1);
+      }
+    } else {
+      /* create multichannel output */
+      int inports = (x->x_inports < 1)?1:x->x_inports;
+      ochannels = x->x_rows;
+      x->x_setmultiout(&sp[compat_offset + inports], ochannels);
+    }
+#else
+    pd_error(x, "BUG: multichannel enabled but not compile-time multichannel support");
+    return;
+#endif
+  }
+
+  /* ensure that the matrices have the correct dimension */
+  io = (t_sample **)resizebytes(x->x_io,
+      (x->x_cols + x->x_rows) * sizeof(*x->x_io),
+      (ichannels + ochannels) * sizeof(*x->x_io));
+
+  if(!io) {
+    pd_error(x, "Unable to get memory");
+    return;
+  }
+  x->x_io = io;
+
+  if(!matrix_multilde_resize(x, ochannels, ichannels)) {
+    return;
+  }
 
   if(!x->x_outsumbuf) {
     x->x_outsumbufsize = n;
@@ -767,19 +872,48 @@ static void matrix_multilde_dsp(t_matrix_multilde *x, t_signal **sp)
     x->x_outsumbufsize = n;
   }
 
-  n = ichannels + ochannels;
-  for(i=0; i<n; i++) {
-    x->x_io[i] = sp[i+compat_offset]->s_vec;
+  if(x->x_setmultiout) {
+#if CLASS_MULTICHANNEL
+    int offset = compat_offset;
+    /* setup input channels */
+    if(x->x_inports) {
+      for(i=0; i<ichannels; i++) {
+	x->x_io[i] = sp[offset + i]->s_vec;
+      }
+      offset += ichannels;
+    } else {
+      for(i=0; i<ichannels; i++) {
+	x->x_io[i] = sp[offset]->s_vec + i * length;
+      }
+      offset += 1;
+    }
+    /* setup output channels */
+    if(x->x_outports) {
+      for(i=0; i<ochannels; i++) {
+	x->x_io[ichannels + i] = sp[offset + i]->s_vec;
+      }
+      offset += ichannels;
+    } else {
+      for(i=0; i<ochannels; i++) {
+	x->x_io[ichannels + i] = sp[offset]->s_vec + i * length;
+      }
+      offset += 1;
+    }
+#endif
+  } else {
+    n = ichannels + ochannels;
+    for(i=0; i<n; i++) {
+      x->x_io[i] = sp[compat_offset+i]->s_vec;
+    }
   }
 
-  n = sp[0]->s_n;
-  x->x_ms2tick = 0.001f * (t_float)(sp[0]->s_sr) / (t_float)n;
+  x->x_ms2tick = 0.001f * (t_float)(sp[0]->s_sr) / (t_float)length;
 
 
-  if(n&7) {
-    dsp_add(matrix_multilde_perform, 2, x, n);
+  if(length&7) {
+    dsp_add(matrix_multilde_perform, 2, x, length);
   } else {
-    dsp_add(matrix_multilde_perf8, 2, x, n);
+    dsp_add(matrix_multilde_perf8, 2, x, length);
   }
 }
 
@@ -788,11 +922,11 @@ static void matrix_multilde_dsp(t_matrix_multilde *x, t_signal **sp)
 
 static void matrix_multilde_free(t_matrix_multilde *x)
 {
-  freebytes(x->x_matcur, x->x_n_in * x->x_n_out * sizeof(t_float));
-  freebytes(x->x_matend, x->x_n_in * x->x_n_out * sizeof(t_float));
-  freebytes(x->x_inc, x->x_n_in * x->x_n_out * sizeof(t_float));
-  freebytes(x->x_biginc, x->x_n_in * x->x_n_out * sizeof(t_float));
-  freebytes(x->x_io, (x->x_n_in + x->x_n_out) * sizeof(t_sample*));
+  freebytes(x->x_matcur, x->x_cols * x->x_rows * sizeof(t_float));
+  freebytes(x->x_matend, x->x_cols * x->x_rows * sizeof(t_float));
+  freebytes(x->x_inc, x->x_cols * x->x_rows * sizeof(t_float));
+  freebytes(x->x_biginc, x->x_cols * x->x_rows * sizeof(t_float));
+  freebytes(x->x_io, (x->x_cols + x->x_rows) * sizeof(t_sample*));
   if(x->x_outsumbuf) {
     freebytes(x->x_outsumbuf, x->x_outsumbufsize * sizeof(t_sample));
   }
@@ -871,24 +1005,16 @@ static void *matrix_multilde_new(t_symbol *s, int argc, t_atom *argv)
 
   /* can we do multichannel? */
   cls = (compat || (!(CLASS_MULTICHANNEL && setmultiout)))?matrix_multilde_class:matrix_multilde_mclass;
+  /* do we need multichannel? */
   if((nin>0) && (nout>0))
     cls = matrix_multilde_class;
 
+  /* create the object */
   x = (t_matrix_multilde *)pd_new(cls);
   if(compat) {
     pd_error(x, "[%s] is deprecated! use [mtx_*~] instead!!", s->s_name);
   }
-
-  x->x_proxy = (t_proxy*)pd_new(matrix_multilde_proxy);
-  x->x_proxy->p_owner = x;
-  pd_bind(&x->x_proxy->p_obj.ob_pd, gensym("pd-dsp-stopped"));
-
   x->x_compat=compat;
-
-  x->x_n_in = nin;
-  x->x_n_out = nout;
-  x->x_time_ms = interpoltime;
-
   x->x_setmultiout = (matrix_multilde_mclass == cls)?setmultiout:0;
 
   if (!compat && ((nin < 1) || (nout < 1))) {
@@ -908,29 +1034,35 @@ static void *matrix_multilde_new(t_symbol *s, int argc, t_atom *argv)
     }
     warn_multichannel = 0;
   }
+  if(!x->x_setmultiout) {
+    if(nin < 1) nin = 1;
+    if(nout < 1) nout = 1;
+  }
 
+  x->x_proxy = (t_proxy*)pd_new(matrix_multilde_proxy);
+  x->x_proxy->p_owner = x;
+  pd_bind(&x->x_proxy->p_obj.ob_pd, gensym("pd-dsp-stopped"));
+
+
+  x->x_inports = nin;
+  x->x_outports = nout;
+  x->x_time_ms = interpoltime;
+
+  x->x_cols = (nin>1)?nin:1;
+  x->x_rows = (nout>1)?nout:1;
 
   /* sanity check */
   if(x->x_time_ms < 0.0f) {
     x->x_time_ms = (x->x_compat==1)?50.f:0.0f;
   }
 
-  if(x->x_n_in < 1) {
-    x->x_n_in = 1;
-  }
-  if(x->x_n_out < 1) {
-    x->x_n_out = 1;
-  }
-
   /* creating signal ins & outs */
-  i = x->x_n_in;
-  if(x->x_compat) {
-    i--;
-  }
+  /* in compat mode, the 1st signal inlet is already made */
+  i = ((x->x_inports>0)?x->x_inports:1) - !!x->x_compat;
   while(i--) {
     inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
   }
-  i = x->x_n_out;
+  i = (x->x_outports>0)?x->x_outports:1;
   while(i--) {
     outlet_new(&x->x_obj, &s_signal);
   }
@@ -945,33 +1077,13 @@ static void *matrix_multilde_new(t_symbol *s, int argc, t_atom *argv)
     inlet_new(&x->x_obj,  &x->x_obj.ob_pd, &s_float, gensym("time"));
   }
 
-
   /* setting up internal values */
-  x->x_msi = 0;
-  x->x_outsumbuf = (t_sample *)0;
-  x->x_outsumbufsize = 0;
-  x->x_matcur = (t_float *)getbytes(x->x_n_in * x->x_n_out * sizeof(
-                                      t_float));
-  x->x_matend = (t_float *)getbytes(x->x_n_in * x->x_n_out * sizeof(
-                                      t_float));
-  x->x_inc = (t_float *)getbytes(x->x_n_in * x->x_n_out * sizeof(t_float));
-  x->x_biginc = (t_float *)getbytes(x->x_n_in * x->x_n_out * sizeof(
-                                      t_float));
-  x->x_io = (t_sample **)getbytes((x->x_n_in + x->x_n_out) * sizeof(
-                                   t_sample*));
+  x->x_matcur = (t_float *)getbytes(x->x_cols * x->x_rows * sizeof(*x->x_matcur));
+  x->x_matend = (t_float *)getbytes(x->x_cols * x->x_rows * sizeof(*x->x_matend));
+  x->x_inc = (t_float *)getbytes(x->x_cols * x->x_rows * sizeof(*x->x_inc));
+  x->x_biginc = (t_float *)getbytes(x->x_cols * x->x_rows * sizeof(*x->x_biginc));
+  x->x_io = (t_sample **)getbytes((x->x_cols + x->x_rows) * sizeof(*x->x_io));
 
-
-  x->x_remaining_ticks = 0;
-  x->x_retarget = 0;
-
-  /* setting up internal matrices */
-  n = x->x_n_in * x->x_n_out;
-  for(i=0; i<n; i++) {
-    x->x_matcur[i] = 0.0f;
-    x->x_matend[i] = 0.0f;
-    x->x_inc[i] = 0.0f;
-    x->x_biginc[i] = 0.0f;
-  }
   return (x);
 }
 

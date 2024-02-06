99eb2083346246be6c5424ef225d0a7f0527e612
IOhannes m zmölnig <zmoelnig@iem.at>
2024-02-06 17:36:14 +0100

    mtx_pack~: only output data if it's non-empty

--- src/mtx_pack~.c
+++ src/mtx_pack~.c
@@ -23,8 +23,11 @@ typedef struct _mtx_pack_tilde {
 } mtx_pack_tilde;
 
 static void mTxPackTildeOut(mtx_pack_tilde*x) {
-  outlet_anything(x->message_outlet,gensym("matrix"),
-                  x->block_size*x->num_channels+2,x->list_out);
+  if(x->block_size * x->num_channels) {
+    outlet_anything(x->message_outlet,gensym("matrix"),
+                    x->block_size*x->num_channels + 2,
+                    x->list_out);
+  }
 }
 void *newMtxPackTilde (t_floatarg f)
 {

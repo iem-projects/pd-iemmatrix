7b5449ffeaab4148cf1ad016c45f4dbec9ffd506
Sourena Mosleh <sourena.mosleh@student.kug.ac.at>
2024-06-05 15:42:06 +0200

    dividing file into array functionality and convolver test

---
+++ test_convolver.c
@@ -0,0 +1,114 @@
+#include "fftw/fftw3.h"
+#include "array.h"
+#include "convolver.h"
+
+
+/* MAIN TESTING ROUTINE */
+/*-----------------------------------------------------------------------------------------------------------------------------*/
+int main() {
+
+     
+    float **in;
+    float ***inh;
+    float **out; 
+    float **target;
+    float *targettemp;
+    float **intemp;
+    float **outtemp;
+    float e;
+
+    int M=5;
+    int L=6;
+    int P=3;
+    int Hann_len=L;
+    int INPUT=2;
+    int OUTPUT=5;
+
+    
+    conv_data conv=initConvolution(L,P,Hann_len,INPUT,OUTPUT);
+    
+
+    //printf("hello!");
+    in = new2DArray(conv.INPUT_Channel_Number,conv.L*M);
+    inh = new3DArray(conv.OUTPUT_Channel_Number,conv.INPUT_Channel_Number,conv.L*conv.P);
+    out = new2DArray(conv.OUTPUT_Channel_Number,conv.L*M);
+    target = new2DArray(conv.OUTPUT_Channel_Number,conv.L*M);
+    targettemp = new1DArray(conv.L*M);
+
+    intemp = new2DArray(conv.INPUT_Channel_Number,conv.L);
+    outtemp = new2DArray(conv.OUTPUT_Channel_Number,conv.L);
+
+    
+    /*reset in ,inh, out and target*/
+
+    reset3DArray(inh,conv.OUTPUT_Channel_Number,conv.INPUT_Channel_Number,conv.L*M);  
+    reset2DArray(in,conv.INPUT_Channel_Number,conv.L*M);
+    reset2DArray(intemp,conv.INPUT_Channel_Number,conv.L);
+    reset2DArray(out,conv.OUTPUT_Channel_Number,conv.L*M);
+    reset2DArray(target,conv.OUTPUT_Channel_Number,conv.L*M);
+    reset2DArray(outtemp,conv.OUTPUT_Channel_Number,conv.L);   
+
+    
+    for(int out_ch=0; out_ch<conv.OUTPUT_Channel_Number;out_ch++) {
+        for(int in_ch=0; in_ch<conv.INPUT_Channel_Number;in_ch++) {
+            //for(int x_imp=0;x_imp<conv.L*M;x_imp++) 
+            {
+                int x_imp=0;
+                //unitImpulse2D(in,conv.INPUT_Channel_Number,conv.L*M,in_ch,x_imp);
+                Rectangle2D(in,conv.INPUT_Channel_Number,conv.L*M,in_ch);
+                //for(int h_imp=0;h_imp<conv.L*conv.P;h_imp++) {
+                    int h_imp=0;
+                    //unitImpulse3D(inh,conv.OUTPUT_Channel_Number,conv.INPUT_Channel_Number,conv.L*conv.P, 
+				    //out_ch, in_ch, h_imp);
+                    //unitImpulse2D(target,conv.OUTPUT_Channel_Number,conv.L*M,
+				    //out_ch,x_imp+h_imp);
+                    Rectangle2D(target,conv.OUTPUT_Channel_Number,conv.L*M,
+				    out_ch);
+
+                    
+                    reset2DArray(conv.x_old,conv.INPUT_Channel_Number,conv.L);
+                    reset3DComplexArray(conv.xf,conv.INPUT_Channel_Number,conv.P,conv.L+1);
+                    int counter=0;
+                    for (int m=0; m<M; m++)
+                    {
+                        
+                        if(counter%NUM_CF==0)
+                        unitImpulse3D(inh,conv.OUTPUT_Channel_Number,conv.INPUT_Channel_Number,conv.L*conv.P,out_ch, in_ch, h_imp);
+                        else
+                        reset3DArray(inh,conv.OUTPUT_Channel_Number,conv.INPUT_Channel_Number,conv.L*M);
+
+                        setImpulseResponse(&conv,inh);
+                        printf("convolver switch after set:%d \n",conv.convolver_switch);
+                        copy2DArray(in, intemp, conv.INPUT_Channel_Number, m*conv.L, 0, conv.L);
+                        conv_process(&conv, intemp, outtemp);
+                        printf("convolver switch after process:%d \n",conv.convolver_switch);
+                        copy2DArray(outtemp, out, conv.OUTPUT_Channel_Number, 0, m*conv.L, conv.L);  
+                        counter++;
+                    }
+                    if ((e=fabsf(conv.OUTPUT_Channel_Number*squaredArray(conv.w,conv.L)-squared2DError(out,target,conv.OUTPUT_Channel_Number,conv.L)))>0.1f)
+                    {
+                        printf("error i:%d,o:%d,xt:%d,ht:%d,e:%f\n",in_ch,out_ch,x_imp,h_imp,e);
+                        //print2DSignal(out,conv.OUTPUT_Channel_Number,conv.L*M);
+                        //print2DSignal(target,conv.OUTPUT_Channel_Number,conv.L*M);
+                    }
+                    else
+                    {
+                        printf("no error i:%d,o:%d,xt:%d,ht:%d,e:%f\n",in_ch,out_ch,x_imp,h_imp,e);
+                    }
+                        print2DSignal(out,conv.OUTPUT_Channel_Number,conv.L*M);
+                        print2DSignal(target,conv.OUTPUT_Channel_Number,conv.L*M);
+                        print_signal(conv.w, conv.L);
+                //}
+
+            }
+            
+        }
+        
+
+    }
+
+    freeConvolution(conv); 
+    
+    return 0;
+}
+

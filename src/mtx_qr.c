c72c96eadeb356bc10c33f54778e462533e41434
IOhannes m zmölnig <zmoelnig@iem.at>
2025-06-10 14:28:18 +0200

    [mtx_qr] dynamic loading of GSL

--- src/mtx_qr.c
+++ src/mtx_qr.c
@@ -14,23 +14,37 @@
  */
 
 #include "iemmatrix.h"
+#include "iemmatrix_stub.h"
 #include <stdlib.h>
 
 #ifdef HAVE_LIBGSL
 #include <gsl/gsl_linalg.h>
+#else
+# include "stub/gsl.h"
 #endif
+
+IEMMATRIX_DECLARE_ALLOCFREE_STUB(my_vector);
+IEMMATRIX_DECLARE_ALLOCFREE2_STUB(my_matrix);
+typedef int(*t_linalg_QR_decomp)(gsl_matrix*, gsl_vector*);
+typedef int(*t_linalg_QR_unpack)(gsl_matrix*, gsl_vector*, gsl_matrix*, gsl_matrix*);
+
+static t_linalg_QR_decomp my_linalg_QR_decomp = 0;
+static t_linalg_QR_unpack my_linalg_QR_unpack = 0;
+
+static int have_gsl = 0;
+
 
 static t_class *mtx_qr_class;
 
 typedef struct _MTXQr_ MTXQr;
 struct _MTXQr_ {
   t_object x_obj;
-#ifdef HAVE_LIBGSL
+
   gsl_matrix *a;
   gsl_vector *tau;
   gsl_matrix *q;
   gsl_matrix *r;
-#endif
+
   t_outlet *list_q_out;
   t_outlet *list_r_out;
   t_atom *list_q;
@@ -39,14 +53,13 @@ struct _MTXQr_ {
   int columns;
 };
 
-#ifdef HAVE_LIBGSL
 static void allocMTXqr (MTXQr *x)
 {
-  x->a=(gsl_matrix*)gsl_matrix_alloc(x->rows,x->columns);
-  x->tau=(gsl_vector*)gsl_vector_alloc(
+  x->a=(gsl_matrix*)my_matrix_alloc(x->rows,x->columns);
+  x->tau=(gsl_vector*)my_vector_alloc(
            ((x->columns<x->rows)?x->columns:x->rows));
-  x->q=(gsl_matrix*)gsl_matrix_alloc(x->rows,x->rows);
-  x->r=(gsl_matrix*)gsl_matrix_alloc(x->rows,x->columns);
+  x->q=(gsl_matrix*)my_matrix_alloc(x->rows,x->rows);
+  x->r=(gsl_matrix*)my_matrix_alloc(x->rows,x->columns);
   x->list_q=(t_atom*)calloc(sizeof(t_atom),x->rows*x->rows+2);
   x->list_r=(t_atom*)calloc(sizeof(t_atom),x->rows*x->columns+2);
 }
@@ -62,32 +75,34 @@ static void deleteMTXqr (MTXQr *x)
 
   x->list_q = x->list_r = 0;
 
-  if (x->a!=0) {
-    gsl_matrix_free(x->a);
-  }
-  if (x->tau!=0) {
-    gsl_vector_free(x->tau);
+  if(have_gsl) {
+    if (x->a!=0) {
+      my_matrix_free(x->a);
+    }
+    if (x->tau!=0) {
+      my_vector_free(x->tau);
+    }
+
+    if (x->q!=0) {
+      my_matrix_free(x->q);
+    }
+    if (x->r!=0) {
+      my_matrix_free(x->r);
+    }
   }
 
-  if (x->q!=0) {
-    gsl_matrix_free(x->q);
-  }
-  if (x->r!=0) {
-    gsl_matrix_free(x->r);
-  }
+  x->list_q = 0;
+  x->list_r = 0;
 
   x->a = 0;
   x->tau = 0;
   x->q = 0;
   x->r = 0;
 }
-#endif
 
 static void deleteMTXQr (MTXQr *x)
 {
-#ifdef HAVE_LIBGSL
   deleteMTXqr(x);
-#endif
 }
 
 static void *newMTXQr (t_symbol *s, int argc, t_atom *argv)
@@ -95,15 +110,17 @@ static void *newMTXQr (t_symbol *s, int argc, t_atom *argv)
   MTXQr *x = (MTXQr *) pd_new (mtx_qr_class);
   x->list_q_out = outlet_new (&x->x_obj, gensym("matrix"));
   x->list_r_out = outlet_new (&x->x_obj, gensym("matrix"));
-  x->list_q = 0;
-  x->list_r = 0;
-#ifdef HAVE_LIBGSL
-  x->a=0;
-  x->q=0;
-  x->r=0;
-  x->tau=0;
-#endif
 
+  if (!have_gsl) {
+    static int warn_gsl = 1;
+    if(warn_gsl)
+#ifdef HAVE_LIBGSL
+      pd_error(x, "[%s] couldn't find (recent enough) GSL", s->s_name);
+#else
+      pd_error(x, "[%s] compiled without GSL", s->s_name);
+#endif
+    warn_gsl = 0;
+  }
   return ((void *) x);
 }
 
@@ -121,13 +138,14 @@ static void mTXQrMatrix (MTXQr *x, t_symbol *s,
 {
   int rows, columns, size;
   int m,n;
+  if(!have_gsl)
+    return;
 
   if(iemmatrix_check(x, argc, argv, 0))return;
   rows = atom_getint (argv++);
   columns = atom_getint (argv++);
   size = rows * columns;
 
-#ifdef HAVE_LIBGSL
   x->rows=rows;
   x->columns=columns;
 
@@ -138,8 +156,8 @@ static void mTXQrMatrix (MTXQr *x, t_symbol *s,
     x->a->data[n]=(double) atom_getfloat(argv++);
   }
 
-  gsl_linalg_QR_decomp(x->a,x->tau);
-  gsl_linalg_QR_unpack(x->a,x->tau, x->q, x->r);
+  my_linalg_QR_decomp(x->a,x->tau);
+  my_linalg_QR_unpack(x->a,x->tau, x->q, x->r);
 
   SETFLOAT((x->list_q),(float) x->rows);
   SETFLOAT((x->list_q+1),(float) x->rows);
@@ -160,9 +178,6 @@ static void mTXQrMatrix (MTXQr *x, t_symbol *s,
   }
 
   mTXQrBang(x);
-#else
-  pd_error(x, "[mtx_qr]: implementation requires gsl");
-#endif
 }
 
 void mtx_qr_setup (void)
@@ -176,6 +191,21 @@ void mtx_qr_setup (void)
   class_addbang (mtx_qr_class, (t_method) mTXQrBang);
   class_addmethod (mtx_qr_class, (t_method) mTXQrMatrix, gensym("matrix"),
                    A_GIMME,0);
+
+#ifdef HAVE_LIBGSL
+  my_matrix_alloc = iemmatrix_get_stub("gsl_matrix_alloc", mtx_qr_class);
+  my_matrix_free = iemmatrix_get_stub("gsl_matrix_free", mtx_qr_class);
+  my_vector_alloc = iemmatrix_get_stub("gsl_vector_complex_alloc", mtx_qr_class);
+  my_vector_free = iemmatrix_get_stub("gsl_vector_complex_free", mtx_qr_class);
+  my_linalg_QR_decomp = iemmatrix_get_stub("gsl_linalg_QR_decomp", mtx_qr_class);
+  my_linalg_QR_unpack = iemmatrix_get_stub("gsl_linalg_QR_unpack", mtx_qr_class);
+#endif
+  have_gsl = (
+              my_matrix_alloc && my_matrix_free
+              && my_vector_alloc && my_vector_free
+              && my_linalg_QR_decomp
+              && my_linalg_QR_unpack
+              );
 }
 
 void iemtx_qr_setup(void)

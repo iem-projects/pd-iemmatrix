fafd8959b2a459471502ab590337b923456f2b46
IOhannes m zmölnig <zmoelnig@iem.at>
2025-07-03 11:01:46 +0200

    tests: more liberal quoting of variables

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 
-: ${PD:=pd}
-: ${PDARGS:=-noprefs -nosound -nrt}
+: "${PD:=pd}"
+: "${PDARGS:=-noprefs -nosound -nrt}"
 
-: ${libdir:=..}
-: ${absdir:=../abs}
+: "${libdir:=..}"
+: "${absdir:=../abs}"
 
-: ${IEMMATRIX:=-lib "${libdir}/iemmatrix" -path "${absdir}"}
+: "${IEMMATRIX:=-lib "${libdir}/iemmatrix" -path "${absdir}"}"
 
 RUNTESTS_TXT=runtests.txt
 runtests_log=runtests.log
@@ -15,18 +15,18 @@ XITCODE=0
 
 for f in */*.pd; do
   test -e "${f}" && echo "${f%.pd};"
-done | LC_ALL=C sort > $RUNTESTS_TXT
+done | LC_ALL=C sort > "${RUNTESTS_TXT}"
 
 run_nogui() {
- echo ${PD} ${PDARGS} $IEMMATRIX -nogui runtests_nogui.pd > ${RUNTESTS_LOG} 2>&1
- ${PD} ${PDARGS} $IEMMATRIX -nogui runtests_nogui.pd >> ${RUNTESTS_LOG} 2>&1
- NUMTESTS=$(grep -c . $RUNTESTS_TXT)
- echo "regression-test: ${NUMTESTS} tests total" >>  ${RUNTESTS_LOG}
+ echo ${PD} ${PDARGS} ${IEMMATRIX} -nogui runtests_nogui.pd > "${RUNTESTS_LOG}" 2>&1
+ ${PD} ${PDARGS} ${IEMMATRIX} -nogui runtests_nogui.pd >> "${RUNTESTS_LOG}" 2>&1
+ NUMTESTS=$(grep -c . "${RUNTESTS_TXT}")
+ echo "regression-test: ${NUMTESTS} tests total" >>  "${RUNTESTS_LOG}"
 
- cat ${RUNTESTS_LOG} \
+ cat "${RUNTESTS_LOG}" \
      | grep -E "^regression-test: " \
      | sed -e 's/^regression-test: //'
- FAILEDTESTS=$(cat ${RUNTESTS_LOG} \
+ FAILEDTESTS=$(cat "${RUNTESTS_LOG}" \
                    | grep -E "^regression-test: .*: failed$" \
                    | sed -e 's|^regression-test: ||' -e 's|: failed$||')
  echo "failed tests: ${FAILEDTESTS}"
@@ -36,7 +36,7 @@ run_nogui() {
 }
 
 run_withgui() {
- ${PD} ${PDARGS} $IEMMATRIX -stderr runtests.pd 2>&1 | tee ${RUNTESTS_LOG}
+ ${PD} ${PDARGS} ${IEMMATRIX} -stderr runtests.pd 2>&1 | tee "${RUNTESTS_LOG}"
 }
 
 if test "$1" = "-gui"; then

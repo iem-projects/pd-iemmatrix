1227d3596fd7be8bd84db8687209d5e149e91f9b
IOhannes m zmölnig <zmoelnig@umlautT.umlaeute.mur.at>
2025-07-07 23:18:38 +0200

    fix sed replace error

--- src/mtx_bitand.c
+++ src/mtx_bitand.c
@@ -1,13 +1,13 @@
 /*
  *  iemmatrix
  *
- *  objects fand manipulating simple matrices
+ *  objects for manipulating simple matrices
  *  mostly referring to matlab/octave matrix functions
  *
  * Copyright (c) IOhannes m zmölnig, fbitandum::für::umläute
  * IEM, Graz, Austria
  *
- * Fand infandmation on usage and redistribution, and fand a DISCLAIMER OF ALL
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
  * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
  *
  */

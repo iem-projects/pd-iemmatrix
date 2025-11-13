078c7fa0587f92da51fbf318a1712f1ba7fd694e
IOhannes m zmölnig <zmoelnig@iem.at>
2025-11-13 14:27:34 +0100

    mtx_fft: cleanup a bit

--- src/mtx_fft.c
+++ src/mtx_fft.c
@@ -20,53 +20,46 @@ static t_class *mtx_fft_class;
 typedef struct _MtxFFT_ MtxFFT;
 struct _MtxFFT_ {
   t_object x_obj;
-  int size;
 
   t_float *f_re;
   t_float *f_im;
 
-  t_outlet *list_re_out;
-  t_outlet *list_im_out;
+  t_outlet *msg_re_out;
+  t_outlet *msg_im_out;
 
-  t_atom *list_re;
-  t_atom *list_im;
+  t_atom *msg_re;
+  t_atom *msg_im;
+  int msg_size;
 };
 
 static void deleteMtxFFT (MtxFFT *x)
 {
-  if (x->f_re) {
-    free (x->f_re);
-  }
-  if (x->f_im) {
-    free (x->f_im);
-  }
-  if (x->list_re) {
-    free (x->list_re);
-  }
-  if (x->list_im) {
-    free (x->list_im);
-  }
+  if (x->f_re)   free (x->f_re);
+  if (x->f_im)   free (x->f_im);
+  if (x->msg_re) free (x->msg_re);
+  if (x->msg_im) free (x->msg_im);
 }
 
 static void *newMtxFFT ()
 {
   MtxFFT *x = (MtxFFT *) pd_new (mtx_fft_class);
   inlet_new(&x->x_obj, &x->x_obj.ob_pd, gensym("matrix"),gensym(""));
-  x->list_re_out = outlet_new (&x->x_obj, gensym("matrix"));
-  x->list_im_out = outlet_new (&x->x_obj, gensym("matrix"));
+  x->msg_re_out = outlet_new (&x->x_obj, gensym("matrix"));
+  x->msg_im_out = outlet_new (&x->x_obj, gensym("matrix"));
 
-  x->size=0;
+  x->msg_size=0;
+  x->msg_re=x->msg_im=0;
+
   x->f_re=x->f_im=0;
-  x->list_re=x->list_im=0;
 
   return ((void *) x);
 }
 
 static void mtxFFTBang (MtxFFT *x)
 {
-  if (x->list_im) {
-    outlet_anything(x->list_im_out, gensym("matrix"), x->size, x->list_im);
-    outlet_anything(x->list_re_out, gensym("matrix"), x->size, x->list_re);
+  if (x->msg_im) {
+    outlet_anything(x->msg_im_out, gensym("matrix"), x->msg_size, x->msg_im);
+    outlet_anything(x->msg_re_out, gensym("matrix"), x->msg_size, x->msg_re);
   }
 }
 
@@ -74,8 +67,8 @@ static void mtxFFTMatrixCold (MtxFFT *x, t_symbol *s,
                               int argc, t_atom *argv)
 {
   int rows, columns, size;
-  t_atom *list_re = x->list_re;
-  t_atom *list_im = x->list_im;
+  t_atom *msg_re = x->msg_re;
+  t_atom *msg_im = x->msg_im;
   t_float *f_re = x->f_re;
   t_float *f_im = x->f_im;
   (void)s; /* unused */
@@ -88,37 +81,39 @@ static void mtxFFTMatrixCold (MtxFFT *x, t_symbol *s,
 
   if (columns < 4) {
     pd_error(x, "[mtx_fft]: matrix must have at least 4 columns");
-  } else if (columns == (1 << ilog2(columns))) {
-    /* ok, prepare real-part of FFT! */
-
-    /* memory things */
-    f_re=(t_float*)realloc(f_re, sizeof(t_float)*size);
-    f_im=(t_float*)realloc(f_im, sizeof(t_float)*size);
-    list_re=(t_atom*)realloc(list_re, sizeof(t_atom)*(size+2));
-    list_im=(t_atom*)realloc(list_im, sizeof(t_atom)*(size+2));
-
-    x->size = size;
-    x->list_im = list_im;
-    x->list_re = list_re;
-    x->f_re = f_re;
-    x->f_im = f_im;
-
-    /* main part */
-    iemmatrix_list2floats(f_im, argv, size);
-
-  } else {
+    return;
+  } else if (columns != (1 << ilog2(columns))) {
     pd_error(x, "[mtx_fft]: rowvector size no power of 2!");
+    return;
   }
+
+  /* ok, prepare real-part of FFT! */
+
+
+  /* memory things */
+  f_re=(t_float*)realloc(f_re, sizeof(t_float)*size);
+  f_im=(t_float*)realloc(f_im, sizeof(t_float)*size);
+  msg_re=(t_atom*)realloc(msg_re, sizeof(t_atom)*(size+2));
+  msg_im=(t_atom*)realloc(msg_im, sizeof(t_atom)*(size+2));
+
+  x->msg_size = size;
+  x->msg_im = msg_im;
+  x->msg_re = msg_re;
+  x->f_re = f_re;
+  x->f_im = f_im;
+
+  /* main part */
+  iemmatrix_list2floats(f_im, argv, size);
 }
 
 
 static void mtxFFTMatrixHot (MtxFFT *x, t_symbol *s,
                              int argc, t_atom *argv)
 {
-  int rows, columns, size;
+  int rows, columns, msg_size;
   int fft_count;
-  t_atom *list_re = x->list_re;
-  t_atom *list_im = x->list_im;
+  t_atom *msg_re = x->msg_re;
+  t_atom *msg_im = x->msg_im;
   t_float *f_re = x->f_re;
   t_float *f_im = x->f_im;
   (void)s; /* unused */
@@ -127,48 +122,49 @@ static void mtxFFTMatrixHot (MtxFFT *x, t_symbol *s,
   if(iemmatrix_check(x, s, argc, argv, 0))return;
   rows = atom_getint (argv++);
   columns = atom_getint (argv++);
-  size = rows * columns;
+  msg_size = rows * columns;
 
-  if (size != x->size) {
+  if (msg_size != x->msg_size) {
     pd_error(x, "[mtx_fft]: left matrix has other dimensions than right matrix");
+    return;
   } else if (columns < 4) {
     pd_error(x, "[mtx_fft]: matrix must have at least 4 columns");
-  } else if (columns == (1 << ilog2(columns))) {
-    /* ok, do the FFT! */
-
-    /* main part */
-    iemmatrix_list2floats(f_re, argv, size);
-
-    fft_count = rows;
-    list_re += 2;
-    list_im += 2;
-    while (fft_count--) {
-      mayer_fft (columns, f_re, f_im);
-      iemmatrix_floats2list(list_re, f_re, columns);
-      iemmatrix_floats2list(list_im, f_im, columns);
-      f_im += columns;
-      f_re += columns;
-      list_re += columns;
-      list_im += columns;
-    }
-
-    list_re = x->list_re;
-    list_im = x->list_im;
-
-    SETSYMBOL(list_re, gensym("matrix"));
-    SETSYMBOL(list_im, gensym("matrix"));
-    SETFLOAT(list_re, rows);
-    SETFLOAT(list_im, rows);
-    SETFLOAT(list_re+1, columns);
-    SETFLOAT(list_im+1, columns);
-    outlet_anything(x->list_im_out, gensym("matrix"),
-                    x->size+2, list_im);
-    outlet_anything(x->list_re_out, gensym("matrix"),
-                    x->size+2, list_re);
-  } else {
+    return;
+  } else if (columns != (1 << ilog2(columns))) {
     pd_error(x, "[mtx_fft]: rowvector size no power of 2!");
+    return;
+  }
+  /* ok, do the FFT! */
+
+  /* main part */
+  iemmatrix_list2floats(f_re, argv, msg_size);
+
+  fft_count = rows;
+  msg_re += 2;
+  msg_im += 2;
+  while (fft_count--) {
+    mayer_fft (columns, f_re, f_im);
+    iemmatrix_floats2list(msg_re, f_re, columns);
+    iemmatrix_floats2list(msg_im, f_im, columns);
+    f_im += columns;
+    f_re += columns;
+    msg_re += columns;
+    msg_im += columns;
   }
 
+  msg_re = x->msg_re;
+  msg_im = x->msg_im;
+
+  SETSYMBOL(msg_re, gensym("matrix"));
+  SETSYMBOL(msg_im, gensym("matrix"));
+  SETFLOAT(msg_re, rows);
+  SETFLOAT(msg_im, rows);
+  SETFLOAT(msg_re+1, columns);
+  SETFLOAT(msg_im+1, columns);
+  outlet_anything(x->msg_im_out, gensym("matrix"),
+                  x->msg_size+2, msg_im);
+  outlet_anything(x->msg_re_out, gensym("matrix"),
+                  x->msg_size+2, msg_re);
 }
 
 void mtx_fft_setup (void)

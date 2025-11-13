8c258f9177fcd38bdba207320d64aea3832549d7
IOhannes m zmölnig <zmoelnig@iem.at>
2025-11-13 15:18:03 +0100

    [mtx_fft] using new FFT interface

--- src/mtx_fft.c
+++ src/mtx_fft.c
@@ -13,6 +13,8 @@
  */
 
 #include "iemmatrix.h"
+#include "iemmatrix_fft.h"
+
 #include <stdlib.h>
 
 static t_class *mtx_fft_class;
@@ -20,12 +22,14 @@ static t_class *mtx_fft_class;
 typedef struct _MtxFFT_ MtxFFT;
 struct _MtxFFT_ {
   t_object x_obj;
-
-  t_float *f_re;
-  t_float *f_im;
-
   t_outlet *msg_re_out;
   t_outlet *msg_im_out;
+
+  t_float *f_re, *f_im;
+  int rows, columns;
+
+  t_iemmatrix_fft_plan*plan;
+  t_complex*c_in, *c_out;
 
   t_atom *msg_re;
   t_atom *msg_im;
@@ -36,6 +40,11 @@ static void deleteMtxFFT (MtxFFT *x)
 {
   if (x->f_re)   free (x->f_re);
   if (x->f_im)   free (x->f_im);
+
+  if (x->plan)   iemmatrix_fft_destroy_plan(x->plan);
+  if (x->c_in)   free (x->c_in);
+  if (x->c_out)  free (x->c_out);
+
   if (x->msg_re) free (x->msg_re);
   if (x->msg_im) free (x->msg_im);
 }
@@ -47,37 +56,57 @@ static void *newMtxFFT ()
   x->msg_re_out = outlet_new (&x->x_obj, gensym("matrix"));
   x->msg_im_out = outlet_new (&x->x_obj, gensym("matrix"));
 
-  x->msg_size=0;
-  x->msg_re=x->msg_im=0;
+  x->msg_size = 0;
+  x->msg_re = x->msg_im = 0;
 
-  x->f_re=x->f_im=0;
+  x->c_in = x->c_out = 0;
 
   return ((void *) x);
 }
 
 static void mtxFFTBang (MtxFFT *x)
 {
-  if (x->msg_im) {
+  if (x->msg_size) {
     outlet_anything(x->msg_im_out, gensym("matrix"), x->msg_size, x->msg_im);
     outlet_anything(x->msg_re_out, gensym("matrix"), x->msg_size, x->msg_re);
   }
 }
 
+static void resizeFFT(MtxFFT *x, int rows, int columns) {
+  int size = rows * columns;
+
+  x->f_re = (t_float*)realloc(x->f_re, sizeof(t_float)*size);
+  x->f_im = (t_float*)realloc(x->f_im, sizeof(t_float)*size);
+
+  x->c_in = (t_complex*)realloc(x->c_in , sizeof(*x->c_in )*columns);
+  x->c_out= (t_complex*)realloc(x->c_out, sizeof(*x->c_out)*columns);
+
+  if(columns != x->columns) {
+    if(x->plan)
+      iemmatrix_fft_destroy_plan(x->plan);
+    x->plan = iemmatrix_fft_plan_1d(columns, x->c_in, x->c_out);
+  }
+
+  int msgsize = size + 2;
+  x->msg_re=(t_atom*)realloc(x->msg_re, sizeof(*x->msg_re)*msgsize);
+  x->msg_im=(t_atom*)realloc(x->msg_im, sizeof(*x->msg_im)*msgsize);
+  x->msg_size = size;
+
+  x->rows = rows;
+  x->columns = columns;
+}
+
 static void mtxFFTMatrixCold (MtxFFT *x, t_symbol *s,
                               int argc, t_atom *argv)
 {
-  int rows, columns, size;
-  t_atom *msg_re = x->msg_re;
-  t_atom *msg_im = x->msg_im;
-  t_float *f_re = x->f_re;
-  t_float *f_im = x->f_im;
   (void)s; /* unused */
 
   /* fftsize check */
   if(iemmatrix_check(x, s, argc, argv, 0))return;
-  rows = atom_getint (argv++);
-  columns = atom_getint (argv++);
-  size = rows * columns;
+
+  int rows = atom_getint (argv++);
+  int columns = atom_getint (argv++);
+  int size = rows * columns;
 
   if (columns < 4) {
     pd_error(x, "[mtx_fft]: matrix must have at least 4 columns");
@@ -91,19 +120,12 @@ static void mtxFFTMatrixCold (MtxFFT *x, t_symbol *s,
 
 
   /* memory things */
-  f_re=(t_float*)realloc(f_re, sizeof(t_float)*size);
-  f_im=(t_float*)realloc(f_im, sizeof(t_float)*size);
-  msg_re=(t_atom*)realloc(msg_re, sizeof(t_atom)*(size+2));
-  msg_im=(t_atom*)realloc(msg_im, sizeof(t_atom)*(size+2));
-
-  x->msg_size = size;
-  x->msg_im = msg_im;
-  x->msg_re = msg_re;
-  x->f_re = f_re;
-  x->f_im = f_im;
+  if((rows != x->rows) || (columns != x->columns)) {
+    resizeFFT(x, rows, columns);
+  }
 
   /* main part */
-  iemmatrix_list2floats(f_im, argv, size);
+  iemmatrix_list2floats(x->f_im, argv, size);
 }
 
 
@@ -111,11 +133,6 @@ static void mtxFFTMatrixHot (MtxFFT *x, t_symbol *s,
                              int argc, t_atom *argv)
 {
   int rows, columns, msg_size;
-  int fft_count;
-  t_atom *msg_re = x->msg_re;
-  t_atom *msg_im = x->msg_im;
-  t_float *f_re = x->f_re;
-  t_float *f_im = x->f_im;
   (void)s; /* unused */
 
   /* fftsize check */
@@ -136,29 +153,45 @@ static void mtxFFTMatrixHot (MtxFFT *x, t_symbol *s,
   }
   /* ok, do the FFT! */
 
-  /* main part */
-  iemmatrix_list2floats(f_re, argv, msg_size);
+  /* memory things */
+  if((rows != x->rows) || (columns != x->columns)) {
+    resizeFFT(x, rows, columns);
+  }
 
-  fft_count = rows;
-  msg_re += 2;
-  msg_im += 2;
-  while (fft_count--) {
-    mayer_fft (columns, f_re, f_im);
-    iemmatrix_floats2list(msg_re, f_re, columns);
-    iemmatrix_floats2list(msg_im, f_im, columns);
-    f_im += columns;
-    f_re += columns;
-    msg_re += columns;
-    msg_im += columns;
+  /* main part */
+  iemmatrix_list2floats(x->f_re, argv, msg_size);
+
+
+  /* main part */
+  t_float *f_re = x->f_re, *f_im = x->f_im;
+  t_atom *msg_re = x->msg_re + 2, *msg_im = x->msg_im + 2;
+
+  for(int r=0; r < rows; r++) {
+    t_complex*c = x->c_in;
+    for(int col=0; col < columns; col++) {
+      c->re = *f_re++;
+      c->im = *f_im++;
+      c++;
+    }
+    iemmatrix_fft_execute(x->plan);
+    c = x->c_out;
+    for(int col=0; col < columns; col++) {
+      t_float re = c->re, im = c->im;
+      SETFLOAT(msg_re, re);
+      SETFLOAT(msg_im, im);
+
+      c++;
+      msg_re++;
+      msg_im++;
+    }
   }
 
   msg_re = x->msg_re;
   msg_im = x->msg_im;
-
   SETSYMBOL(msg_re, gensym("matrix"));
   SETSYMBOL(msg_im, gensym("matrix"));
-  SETFLOAT(msg_re, rows);
-  SETFLOAT(msg_im, rows);
+  SETFLOAT(msg_re+0, rows);
+  SETFLOAT(msg_im+0, rows);
   SETFLOAT(msg_re+1, columns);
   SETFLOAT(msg_im+1, columns);
   outlet_anything(x->msg_im_out, gensym("matrix"),
@@ -180,6 +213,8 @@ void mtx_fft_setup (void)
                    gensym("matrix"), A_GIMME,0);
   class_addmethod (mtx_fft_class, (t_method) mtxFFTMatrixCold, gensym(""),
                    A_GIMME,0);
+
+  iemmatrix_fft_init(mtx_fft_class);
 }
 
 void iemtx_fft_setup(void)
